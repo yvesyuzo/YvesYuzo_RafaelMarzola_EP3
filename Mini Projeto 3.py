12a6dd4c78b1910f6b7fe7aae499bbc334be37cf
yvesyuzo <yvesyuzo@gmail.com>
2015-04-16 18:40:42 -0300

    Adicionada função função "criador_info_nutricional"
    função que transforma os dados do arquivo .csv em um dicionario onde as
    keys são as comidas e os valores são os dados

--- Mini Projeto 3.py
+++ Mini Projeto 3.py
@@ -21,14 +21,16 @@ for la in leitura:
     if y != "":
         usuario.append(y)
 #--------------------------
-print(usuario)
 
-def info_nutricional(lista_de_alimentos_com_info):
-    info_nutricional = dict
-    for i in range (1,len[lista_de_alimentos_com_info]):
+def criador_info_nutricional(lista_de_alimentos_com_info):
+    info_nutricional = {}    
+    for i in range(1,len(lista_de_alimentos_com_info)):        
         split_da_linha = lista_de_alimentos_com_info[i].split(',')
-        info_nutricional = {split_da_linha[0]: split_da_linha[1:]}
+        info_nutricional[split_da_linha[0]] = split_da_linha[1:]
+        
     return info_nutricional
         
-info_nutricional(alimentos)    
+d = criador_info_nutricional (alimentos)
+print(d['acerola'][3])
+
 
