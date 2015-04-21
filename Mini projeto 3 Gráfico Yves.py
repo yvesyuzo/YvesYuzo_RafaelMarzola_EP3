3d7c3f9fc1c324f9021b83ad97810f3ade09d42b
yvesyuzo <yvesyuzo@gmail.com>
2015-04-21 18:59:19 -0300

    removido partes repetidas

--- Mini projeto 3 Gráfico Yves.py
+++ Mini projeto 3 Gráfico Yves.py
@@ -5,25 +5,6 @@ Created on Tue Apr 21 18:42:23 2015
 @author: Yves Yuzo
 """
 from Mini_Projeto_3 import*
-
-#Criação de uma lista com os valores do arquivo alimentos 
-raw = open("alimentos.csv")
-leitura = raw.readlines()
-alimentos = []
-for pa in leitura:
-    x = pa.strip().lower()
-    if x != "":
-        alimentos.append(x)
-#Criação de uma lista com a info do usuario
-        
-usuario_raw = open("usuario.csv", encoding = "cp1252")
-leitura2 = usuario_raw.readlines()
-usuario = []
-for la in leitura2:
-    y = la.strip().lower()
-    if y != "":
-        usuario.append(y)
-        
         
 
 lista_de_alimentos = criador_info_nutricional (alimentos)
